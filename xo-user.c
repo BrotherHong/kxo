122c9ca9d49fd1927fcc6aaf4e32a8ae32127331
BrotherHong <leo361288@gmail.com>
2025-04-01 22:36:11 +0800

    Add time measurement mode in user program
    Implement time measurement mode to measure
    the time cost on read system call.

--- xo-user.c
+++ xo-user.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/select.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "game.h"
@@ -13,6 +14,12 @@
 #define XO_STATUS_FILE "/sys/module/kxo/initstate"
 #define XO_DEVICE_FILE "/dev/kxo"
 #define XO_DEVICE_ATTR_FILE "/sys/class/kxo/kxo/kxo_state"
+
+#define MEASURE_MODE 1
+#if MEASURE_MODE
+#define MEASURE_COUNT 1000
+#define MEASURED_FILE "/tmp/measured_kernel.txt"
+#endif
 
 static bool status_check(void)
 {
@@ -82,6 +89,10 @@ static void listen_keyboard_handler(void)
     close(attr_fd);
 }
 
+#if MEASURE_MODE
+long measured_time[MEASURE_COUNT];
+#endif
+
 int main(int argc, char *argv[])
 {
     if (!status_check())
@@ -98,6 +109,12 @@ int main(int argc, char *argv[])
     int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
     read_attr = true;
     end_attr = false;
+
+#if MEASURE_MODE
+    int measure_idx = 0;
+    struct timespec start, end;
+    printf("Measuring the time taken to read the display buffer...\n");
+#endif
 
     while (!end_attr) {
         FD_ZERO(&readset);
@@ -116,10 +133,46 @@ int main(int argc, char *argv[])
         } else if (read_attr && FD_ISSET(device_fd, &readset)) {
             FD_CLR(device_fd, &readset);
             printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
+#if MEASURE_MODE
+            int len;
+            long elapsed_time = 0;
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            len = read(device_fd, display_buf, DRAWBUFFER_SIZE);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            if (len > 0) {
+                elapsed_time = (end.tv_sec - start.tv_sec) * 1000000 +
+                               (end.tv_nsec - start.tv_nsec) / 1000;
+
+                // only record the time that is greater than 0.1 ms
+                if (elapsed_time >= 100)
+                    measured_time[measure_idx++] = elapsed_time;
+
+                if (measure_idx >= MEASURE_COUNT)
+                    end_attr = true;
+            }
+
+            printf("%s\n", display_buf);
+            printf("Current measured time: %ld us\n", elapsed_time);
+            printf("Remaining measurements: %d\n", MEASURE_COUNT - measure_idx);
+#else
             read(device_fd, display_buf, DRAWBUFFER_SIZE);
             printf("%s", display_buf);
+#endif
         }
     }
+
+#if MEASURE_MODE
+    FILE *fp = fopen(MEASURED_FILE, "w");
+    if (!fp) {
+        printf("Error opening file %s\n", MEASURED_FILE);
+        exit(1);
+    }
+    for (int i = 0; i < MEASURE_COUNT; i++) {
+        fprintf(fp, "%ld\n", measured_time[i]);
+    }
+#endif
 
     raw_mode_disable();
     fcntl(STDIN_FILENO, F_SETFL, flags);
